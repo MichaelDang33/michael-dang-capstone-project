7f66a89aeacb8326e0302121ad5fa3d2e3b4348c
MichaelDang33 <michaeldang3@gmail.com>
2023-08-16 10:12:18 -0500

    Initial commit

---
+++ my-app/src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./NotFound.scss";
+
+const NotFound = () => {
+  return (
+    <div className='not-found my-5'>
+      <div className='container flex align-center justify-center'>
+        :( Nothing available with your entry..
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
